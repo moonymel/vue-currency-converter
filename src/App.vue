16296d40bc3a3ca0c263efbf176b4393f0e78a8a
moonymel <musumeci.me@gmail.com>
2024-05-07 11:15:45 +0200

    [UPDATE] added input, milestone 1 completed and fixing milestone 2

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,64 @@
 <script>
-export default {
+import axios from 'axios';
+import AppInput from './components/AppInput.vue';
 
+export default {
+  components: {
+    AppInput,
+  },
+  data() {
+    return {
+      apiUrl: 'https://api.frankfurter.app',
+      currencies: [],
+    }
+  },
+  created() {
+    this.getCurrencies();
+  },
+  methods: {
+    getCurrencies() {
+      axios.get(`${this.apiUrl}/currencies`).then((response) => {
+        this.currencies = response.data
+        console.log(this.currencies)
+      })
+    }
+  }
 }
 </script>
 
 <template lang="">
-  <div>
-    
+  <div class="container my-container">
+    <div class="row">
+
+      <div class="col-12">
+        prova
+      </div>
+
+      <div class="col-6">
+        prova2
+      </div>
+
+      <div class="col-6">
+        prova2
+      </div>
+
+      <div class="col-12">
+        <AppInput :currencies="currencies" />
+        <AppInput :currencies="currencies" />
+      </div>
+
+    </div>
   </div>
 </template>
 
-<style lang="">
+<style lang="scss">
+@use './styles/general.scss' as *;
+@use './styles/partials/variables.scss' as *;
+@use './styles/partials/mixins.scss' as *;
 
+.my-container {
+  width: 100%;
+  background-color: grey;
+  border: 1px solid black;
+}
 </style>
---
+++ src/components/AppInput.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  props: {
+    currencies: Object,
+  },
+  data() {
+    return {
+      digit: '',
+    }
+  }
+}
+</script>
+
+<template lang="">
+
+<input type="text" class="form-control" v-model="digit">
+<select name="" id="" class="form-select">
+  <option v-for="(currency, key) in currencies" :value="key" :selected="key === 'EUR' ? true : false">{{ currency }}</option>
+</select>
+
+</template>
+
+<style lang="">
+
+</style>
